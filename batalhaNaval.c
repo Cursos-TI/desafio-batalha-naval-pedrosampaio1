2428d921f8448a90ef038340085b4411a2a9e322
Pedro Sampaio <105110684+pedrosampaio1@users.noreply.github.com>
2025-08-19 22:06:32 +0000

    ajustes na inicialização dos navios

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -24,21 +24,26 @@ int main()
 	}
 	printf("\n");
 
-	// Definindo a posição do navio 1
-	int linha_inicio = 1;
-	int coluna_inicio = 2;
-	for (int i = 0; i < 3; i++)
-	{
-		tabuleiro[linha_inicio + i][coluna_inicio] = 3;
-	}
+	// Navio 1
+    int navio1[3] = {3, 3, 3};
+    // Navio 2
+    int navio2[4] = {3, 3, 3, 3};
 
-	// Definindo a posição do navio 2
-	linha_inicio = 5;
-	coluna_inicio = 5;
-	for (int j = 0; j < 4; j++)
-	{
-		tabuleiro[linha_inicio][coluna_inicio + j] = 3;
-	}
+    // Definindo a posição do navio 1
+    int linha_inicio1 = 1;
+    int coluna_inicio1 = 2;
+
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[linha_inicio1 + i][coluna_inicio1] = navio1[i];
+    }
+
+    // Definindo a posição do navio 2
+    int linha_inicio2 = 5;
+    int coluna_inicio2 = 5;
+
+    for (int j = 0; j < 4; j++) {
+        tabuleiro[linha_inicio2][coluna_inicio2 + j] = navio2[j];
+    }
 
 	// Imprimindo o tabuleiro completo
 	for (int i = 0; i < 10; i++)
